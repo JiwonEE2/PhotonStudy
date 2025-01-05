ab70d48c32fc39fd5b622cd3e7cc98095e010a2e
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-05 22:38:32 +0900

    서버 접속했을 때 메뉴패널 열리도록 수정

--- Assets/_Project/Scripts/Lobby/PanelManager.cs
+++ Assets/_Project/Scripts/Lobby/PanelManager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class PanelManager : MonoBehaviour {
+public class PanelManager : MonoBehaviourPunCallbacks
+{
 	public static PanelManager Instance;
 
 	public LoginPanel login;
@@ -10,29 +12,34 @@ public class PanelManager : MonoBehaviour {
 	public LobbyPanel lobby;
 	public RoomPanel room;
 
-	
+
 	Dictionary<string, GameObject> panelDic;
 
-	private void Awake() {
+	private void Awake()
+	{
 		Instance = this;
 		panelDic = new Dictionary<string, GameObject>()
 		{
-			{ "Login", login.gameObject},
-			{ "Menu", menu.gameObject},
-			{ "Lobby", lobby.gameObject},
+			{ "Login", login.gameObject },
+			{ "Menu", menu.gameObject },
+			{ "Lobby", lobby.gameObject },
 			{ "Room", room.gameObject }
 		};
 
 		PanelOpen("Login");
 	}
 
-	public void PanelOpen(string panelName) {
-		foreach (var row in panelDic) {
+	public void PanelOpen(string panelName)
+	{
+		foreach (var row in panelDic)
+		{
 			row.Value.SetActive(row.Key == panelName);
 		}
 	}
+
+	// photon server에 접속되었을 때 호출
+	public override void OnConnected()
+	{
+		PanelOpen("Menu");
+	}
 }
-	
-
-	
-
