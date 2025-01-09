0beba67ffb7ea275d0b1080c326642cc2ad9dc7c
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-09 19:31:36 +0900

    photon chat 닉네임 설정 및 서버 연결 구현 2

--- Assets/_Project/Scripts/Chat/JoinUI.cs
+++ Assets/_Project/Scripts/Chat/JoinUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,35 @@ public class JoinUI : MonoBehaviour
 {
 	public InputField nicknameInput;
 	public InputField roomnameInput;
+
 	public Button nicknameChangeButton;
-
 	public Button connectButton;
-
 	public Button joinRoomButton;
+
 	public Text logText;
 
-}
+	private void Awake()
+	{
+		nicknameChangeButton.onClick.AddListener(NicknameChangeButtonClick);
+		connectButton.onClick.AddListener(ConnectButtonClick);
+		joinRoomButton.onClick.AddListener(JoinRoomButtonClick);
+	}
 
+	// 유효한 닉네임인지 검증을 위해 string에 한 번 담음 (ㅣㅏ아ㅓ 같은 것들 제외할 예정)
+	private void NicknameChangeButtonClick()
+	{
+		string nickname = nicknameInput.text;
+		ChatManager.Instance.SetNickname(nickname);
+	}
+
+	private void ConnectButtonClick()
+	{
+		ChatManager.Instance.ConnectUsingSettings();
+		// 한 번만 누르도록 제한
+		connectButton.interactable = false;
+	}
+
+	private void JoinRoomButtonClick()
+	{
+	}
+}
