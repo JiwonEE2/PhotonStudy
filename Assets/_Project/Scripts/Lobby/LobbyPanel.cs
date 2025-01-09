9d65f64ec360a1eb16c0a67782408bbf33b2618c
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-09 14:12:00 +0900

    로비 퇴장 구현

--- Assets/_Project/Scripts/Lobby/LobbyPanel.cs
+++ Assets/_Project/Scripts/Lobby/LobbyPanel.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyPanel : MonoBehaviour
 {
 	public RectTransform roomListRect;
-
 	public GameObject roomButtonPrefab;
-
 	public Button cancelButton;
 
 	private void Awake()
 	{
+		cancelButton.onClick.AddListener(CancelButtonClick);
 	}
 
+	private void CancelButtonClick()
+	{
+		PhotonNetwork.LeaveLobby();
+	}
 }
-
