239b9c938506079e9256a820589d2bd5fd398d7b
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-12 23:18:37 +0900

    알약 객체 생성하고 동기화

--- Assets/_Project/Scripts/Game/GameManager.cs
+++ Assets/_Project/Scripts/Game/GameManager.cs
@@ -41,5 +41,30 @@ public class GameManager : MonoBehaviour
 		GameObject playerObj =
 			PhotonNetwork.Instantiate("Player", playerPos, Quaternion.identity);
 		playerObj.name = $"Player {playerNumber}";
+
+		// 이 밑에서는 내가 MasterClient가 아니면 동작하지 않음
+		if (false == PhotonNetwork.IsMasterClient)
+		{
+			yield break;
+		}
+
+		// Master Client만 5초마다 Pill을 PhotonNetwork를 통해 Instantiate
+		while (true)
+		{
+			// PhotonNetwork.Instantiate를 통해 생성할 경우, position과 rotation이 반드시 필요
+			Vector3 spawnPos = Random.insideUnitSphere * 15;
+			spawnPos.y = 0;
+			Quaternion spawnRot = Quaternion.Euler(0, Random.Range(0, 180f), 0);
+
+			// 각 pill마다 random color(Color)와 random healAmount(float)를 주입하고 싶으면?
+			// photon에서는 Color 직렬화를 지원하지 않기 때문에 Vector3로 하면 된다.
+			Vector3 color = new Vector3(Random.value, Random.value, Random.value);
+			float healAmount = Random.Range(10f, 30f);
+
+			PhotonNetwork.Instantiate("Pill", spawnPos, spawnRot,
+				data: new object[] { color, healAmount });
+
+			yield return new WaitForSeconds(5f);
+		}
 	}
 }
--- Assets/_Project/Scripts/Game/Pill.cs
+++ Assets/_Project/Scripts/Game/Pill.cs
@@ -1,13 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class Pill : MonoBehaviour
+public class Pill : MonoBehaviourPun
 {
 	public Renderer render;
 
-	private void Reset() {
+	// 힐량 랜덤
+	private float healAmount;
+
+	private void Reset()
+	{
 		render = GetComponentInChildren<Renderer>();
 	}
-}
 
+	private void Awake()
+	{
+		// PhotonNetwork.Instantiate 호출 시 함께 보낸 data 파라미터
+		object[] param = photonView.InstantiationData;
+
+		if (param != null)
+		{
+			Vector3 cv = (Vector3)param[0];
+			float healAmount = (float)param[1];
+			render.material.color = new Color(cv.x, cv.y, cv.z);
+			this.healAmount = healAmount;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			other.SendMessage("Heal", healAmount);
+		}
+
+		// 각자 클라이언트에서 모두 Destroy
+		Destroy(gameObject);
+	}
+}
