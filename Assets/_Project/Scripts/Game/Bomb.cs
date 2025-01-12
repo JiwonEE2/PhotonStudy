4fe1a9295f91dfe9fbe9fa7a4bb9f11f24e0150c
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-12 18:39:56 +0900

    추측항법을 통해 움직이는 객체 생성 및 지연보상 작업. PunRPC

--- Assets/_Project/Scripts/Game/Bomb.cs
+++ Assets/_Project/Scripts/Game/Bomb.cs
@@ -1,11 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
 	public Rigidbody rb;
 	public ParticleSystem particlePrefab;
+	public Player owner;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// 나중에 Object Pool 사용하면 좋다.
+		ParticleSystem particle =
+			Instantiate(particlePrefab, transform.position, particlePrefab.transform.rotation);
+		particle.Play();
+		Destroy(particle.gameObject, 3);
+
+		// 충돌 즉시 renderer와 collider 비활성화하여 실질적인 역할을 하지 않도록
+		GetComponent<Renderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+
+		Destroy(gameObject, 0.1f);
+
+		// 폭발이 일어나면 조금 더 큰 범위의 원 내에 있는 모든 콜라이더에 데미지
+		Collider[] contactedColliders = Physics.OverlapSphere(transform.position, 1.5f);
+
+		foreach (Collider collider in contactedColliders)
+		{
+			if (collider.tag == "Player")
+			{
+				collider.SendMessage("Hit", 1);
+				PhotonView target = collider.GetComponent<PhotonView>();
+				print($"{owner.NickName}이 던진 폭탄에 {target.Owner.NickName}가 맞음");
+			}
+		}
+	}
 
 	private void Reset()
 	{
--- Assets/_Project/Scripts/Game/PlayerController.cs
+++ Assets/_Project/Scripts/Game/PlayerController.cs
@@ -39,6 +39,7 @@ public class PlayerController : MonoBehaviourPun
 		rb = GetComponent<Rigidbody>();
 		pointer = transform.Find("PlayerPointer");
 		shotPoint = transform.Find("ShotPoint");
+		tag = photonView.IsMine ? "Player" : "Enemy";
 	}
 
 	private void Update()
@@ -46,6 +47,15 @@ public class PlayerController : MonoBehaviourPun
 		// 내 photon view만 움직이도록 예외처리
 		if (false == photonView.IsMine) return;
 		Move();
+		if (Input.GetButtonDown("Fire1"))
+		{
+			// Fire();
+			// 주인도 Fire를 직접 호출하지 않고 호출한다는 신호만 보내 Server에서 받아온다.
+			photonView.RPC("Fire", RpcTarget.All, shotPoint.position,
+				shotPoint.forward);
+			shotCount++;
+			shotText.text = shotCount.ToString();
+		}
 	}
 
 	private void FixedUpdate()
@@ -68,5 +78,41 @@ public class PlayerController : MonoBehaviourPun
 		Vector3 forward = pointer.position - pos;
 
 		rb.rotation = Quaternion.LookRotation(forward, Vector3.up);
+	}
+
+	private void Hit(float damage)
+	{
+		hp -= damage;
+		hpText.text = hp.ToString();
+	}
+
+	private void Heal(float amount)
+	{
+		hp += amount;
+		hpText.text = hp.ToString();
+	}
+
+	// fire를 통해 생성하는 bomb 객체는 "데드레커닝" (추측항법 알고리즘)을 통해 각 클라이언트들이 직접 생성하고,
+	// Fire 함수를 호출받는 시점을 온라인에서 원격으로 호출받음. (Remote Procedure Call)
+	[PunRPC]
+	// PhotonMessageInfo는 무조건 가장 마지막에
+	private void Fire(Vector3 shotPoint, Vector3 shotDir, PhotonMessageInfo info)
+	{
+		print($"Fire Procedure called by {info.Sender.NickName}");
+		print($"my local time: {PhotonNetwork.Time}");
+		print($"server time when procedure called: {info.SentServerTime}");
+
+		// "지연보상": 추측항법을 위해 rpc를 받은 시점은 서버에서 호출된 시간보다 항상 늦기 때문에,
+		// 해당 지연시간만틈 위치, 또는 연산량을 보정해 주어야 최대한 원격에서의 플레이가 동기화될 수 있음.
+
+		// 보정해야할 지연 시간
+		float lag = (float)(PhotonNetwork.Time - info.SentServerTime);
+
+		Bomb bomb = Instantiate(bombPrefab, shotPoint, Quaternion.identity);
+		bomb.rb.AddForce(shotDir * shotPower, ForceMode.Impulse);
+		bomb.owner = photonView.Owner;
+
+		// 지연 보상 시작
+		bomb.rb.position += bomb.rb.velocity * lag;
 	}
 }
