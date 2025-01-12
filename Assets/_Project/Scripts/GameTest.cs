370603c2b0e4a8bdb942949a91885d64f5a71073
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-12 15:41:26 +0900

    입장 시 유저 이름, 연결 상태 로그 찍는 것 구현

---
+++ Assets/_Project/Scripts/GameTest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class GameTest : MonoBehaviour
+{
+	private void Start()
+	{
+		LogManager.Log(PhotonNetwork.NickName);
+		LogManager.Log(PhotonNetwork.IsConnected.ToString());
+	}
+}
