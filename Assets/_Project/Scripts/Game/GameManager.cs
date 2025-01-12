e40899e71191a8b9614b80934a4cb6551bdab264
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-12 17:49:41 +0900

    개발자 모드 구현

--- Assets/_Project/Scripts/Game/GameManager.cs
+++ Assets/_Project/Scripts/Game/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 {
 	public static GameManager Instance { get; private set; }
 	public Transform playerPositions;
+	public static bool isGameReady;
 
 	private void Awake()
 	{
@@ -19,8 +20,12 @@ public class GameManager : MonoBehaviour
 	// 1. 프리팹에 PhotonView 컴포넌트를 붙이고, PhotonNetwork.Instantiate를 통해 원격 클라이언트들에게도 동기화된 오브젝트를 생성하도록 함
 	// 2. PhotonView가 Observing할 수 있도록 View 컴포넌트를 부착
 	// 3. 내 View가 부착되지 않은 오브젝트는 내가 제어하지 않도록 예외처리를 반드시 할 것
-	private void Start()
+	private IEnumerator Start()
 	{
+		yield return new WaitUntil(() => isGameReady);
+		yield return new WaitForSeconds(1f);
+
+		// 랜덤 위치에 플레이어 생성
 		Vector3 spawnPos = playerPositions.GetChild(Random.Range(0, playerPositions
 			.childCount)).position;
 		PhotonNetwork.Instantiate("Player", spawnPos, Quaternion.identity)
---
+++ Assets/_Project/Scripts/Game/PhotonManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+	public bool isTestMode = false;
+
+	private void Start()
+	{
+		isTestMode = PhotonNetwork.IsConnected == false;
+		if (isTestMode)
+		{
+			PhotonNetwork.ConnectUsingSettings();
+		}
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		if (isTestMode)
+		{
+			RoomOptions option = new()
+			{
+				IsVisible = false,
+				MaxPlayers = 8
+			};
+			PhotonNetwork.JoinOrCreateRoom("TestRoom", option, TypedLobby.Default);
+		}
+	}
+
+	public override void OnJoinedRoom()
+	{
+		if (isTestMode)
+		{
+			GameObject.Find("Canvas/DebugText").GetComponent<Text>().text =
+				PhotonNetwork.CurrentRoom.Name;
+			GameManager.isGameReady = true;
+		}
+	}
+}
